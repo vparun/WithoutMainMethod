de2550831ad738847426444fb2045de26cb15fec
Arunkumar Parthasarathy <arunkumar.ps1988@gmail.com>
2023-04-28 17:13:12 +0530

    Added program file

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Personal;
+
+Console.WriteLine("Hello, World!");
+
+var name = "Arun";
+
+// Object creating
+var person = new Person("Arunkumar");
+
+// Display
+Print("HI ");
+
+PrintName("Hi ");
+
+// Functions
+void Print(string text) => Console.WriteLine(text + " " + name);
+
+void PrintName(string text) => Console.WriteLine(text + " " + person.Name);
+
+
+namespace Personal
+{
+    public class Person
+    {
+        public string Name { get; }
+        public Person(string name) => Name = name;
+    }
+}
